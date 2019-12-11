ded4e5be17c511ca76fee9a5f825dbbf21eaeeca
huhui <569263082@qq.com>
2019-12-11 21:23:34 +0800

    fix: 优化

--- src/views/App.vue
+++ src/views/App.vue
@@ -4,6 +4,23 @@
       <div slot="content" class="index">
         <div class="main">
           <div class="context-wrap" hw-animate="fadeInUp">
+            <el-form ref="form" :model="form" label-width="170px" :inline="true" style="margin-top:20px;">
+              <el-form-item label="按照省份选择您的城市">
+                <el-select v-model="form.city" placeholder="请选择您的城市">
+                  <el-option label="重庆" value="cq"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="学校">
+                <el-select v-model="form.school" placeholder="请选择学校">
+                  <el-option label="长江师范学院" value="yznu"></el-option>
+                  <el-option label="文理学院" value="wenli"></el-option>
+                  <el-option label="重庆邮电大学" value="youdian"></el-option>
+                  <el-option label="重庆大学" value="cq"></el-option>
+                  <el-option label="四川美术学院" value="meishu"></el-option>
+                  <el-option label="重庆理工大学" value="ligong"></el-option>
+                </el-select>
+              </el-form-item>
+            </el-form>
             <div class="type-list">
               <div @click="active = 'recruitment'" :class="{active:active === 'recruitment'}">招聘</div>
               <div @click="active = 'apply'" :class="{active:active === 'apply'}">求职</div>
@@ -18,8 +35,7 @@
         </div>
       </div>
     </layouts>
-    <div>
-    </div>
+    <div></div>
   </div>
 </template>
 
@@ -36,10 +52,14 @@ export default {
     marketList,
     applyList
   },
-  data(){
+  data() {
     return {
-      active: "recruitment"
-    }
+      active: "recruitment",
+      form:{
+        city:'cq',
+        school:''
+      }
+    };
   },
   methods: {},
   mounted() {
@@ -56,26 +76,26 @@ export default {
     margin: 20px;
   }
   .index {
-    .main{
+    .main {
       .context-wrap {
-      width: 80%;
-      margin: 0 auto;
-      .type-list {
-        display: flex;
-        text-align: center;
-        min-height: 60px;
-        line-height: 60px;
-        & > div {
-          flex: 1;
+        width: 80%;
+        margin: 0 auto;
+        .type-list {
+          display: flex;
+          text-align: center;
+          min-height: 60px;
+          line-height: 60px;
+          & > div {
+            flex: 1;
+          }
+          .active {
+            border-bottom: 2px solid blue;
+          }
         }
-        .active {
-          border-bottom: 2px solid blue;
+        .box-wrap {
+          min-height: 500px;
         }
       }
-      .box-wrap{
-        min-height: 500px;
-      }
-    }
     }
   }
 }
--- src/views/market/components/marketList.vue
+++ src/views/market/components/marketList.vue
@@ -3,17 +3,26 @@
     <div class="wrap" v-for="item in list" :key="item._id">
       <div class="title">{{item.title}}</div>
       <div class="info">
-        <div>兼职类型{{item.type}}</div>
-        <div>招聘职位{{item.post}}</div>
-        <div>薪资范围{{item.range}}</div>
-        <div>招聘人数{{item.number}}</div>
-        <div>工作时间{{item.fromTime | dateformat}}</div>
-        <div>工作时间{{item.toTime | dateformat}}</div>
-        <div>工作地点{{item.location}}</div>
+        <div class="left">
+          <div>兼职类型：{{item.type}}</div>
+          <div>招聘职位：{{item.post}}</div>
+          <div>薪资范围：{{item.range}}</div>
+          <div>招聘人数：{{item.number}}</div>
+          <div>工作时间：{{item.fromTime | dateformat}}</div>
+          <div>工作时间：{{item.toTime | dateformat}}</div>
+        </div>
+        <div class="right">
+          <div>
+            <i class="el-icon-location" style="color:#409eff"></i>
+            工作地点：{{item.location}}
+          </div>
+        </div>
       </div>
       <div class="footer">
         <div class="detail">
-          <router-link :to="{path:'/marketDetail',query:{ companyId:item.companyId,postId:item.postId}}">查看详情</router-link>
+          <router-link
+            :to="{path:'/marketDetail',query:{ companyId:item.companyId,postId:item.postId}}"
+          >查看详情</router-link>
         </div>
         <div class="time">发布时间:{{item.now}}</div>
       </div>
@@ -24,24 +33,22 @@
 <script>
 export default {
   name: "marketList",
-  data (){
+  data() {
     return {
-      list:[]
-    }
+      list: []
+    };
   },
-  mounted (){
-    this.fetch()
+  mounted() {
+    this.fetch();
   },
-  methods:{
-    fetch: async function(){
+  methods: {
+    fetch: async function() {
       let res = await this.$api.allPost();
-      res.result = [].concat.apply([],res.result)
-      this.list =  res.result.filter((item)=>item !== null)
-    },
-
+      res.result = [].concat.apply([], res.result);
+      this.list = res.result.filter(item => item !== null);
+    }
   }
 };
-
 </script>
 
 <style lang="less" scoped>
@@ -58,6 +65,12 @@ export default {
     font-size: 16px;
     font-weight: bolder;
     color: gray;
+    display: flex;
+    justify-content: space-between;
+    .left {
+    }
+    .right {
+    }
   }
   .footer {
     font-size: 14px;
